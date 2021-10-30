7d6feea0f3941bfcedf8ee07ff3b5a8ea2253e84
carloskauan <carloskauan190@gmail.com>
2021-10-29 22:07:39 -0300

    first commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,8 @@
+//Pacotes
+package com.example.affirmations.model
+
+    //Adicione um parâmetro stringResourceId val inteiro ao construtor da classe Affirmation.
+    //Isso eliminará o erro.
+
+data class Affirmation(val stringResourceID: Int) {
+}
