04d53ac24af9193e355ecb95edb07c034698932f
carloskauan <carloskauan190@gmail.com>
2021-11-06 21:47:54 -0300

    Adicionado imagens do app

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,8 +1,11 @@
 //Pacotes
 package com.example.affirmations.model
 
-    //Adicione um parâmetro stringResourceId val inteiro ao construtor da classe Affirmation.
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+
+//Adicione um parâmetro stringResourceId val inteiro ao construtor da classe Affirmation.
     //Isso eliminará o erro.
 
-data class Affirmation(val stringResourceID: Int) {
-}
+data class Affirmation(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int)
